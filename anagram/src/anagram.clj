0f2d69bcb22c175b4994d5b6273ebfaad4094587
Shrivatsa <shrivatsa.swadi@gmail.com>
2019-09-08 15:23:28 +0530

    Cleaned up the anagram test

--- anagram/src/anagram.clj
+++ anagram/src/anagram.clj
@@ -1,18 +1,10 @@
 (ns anagram
   (:use [clojure.string :only [lower-case]]))
 
-(defn same-word? [word potential-anagram]
-  (and
-      (not= word potential-anagram)
-      (not= (lower-case word) (lower-case potential-anagram))))
-
-(defn same-letters? [word potential-anagram]
-  (= (sort (lower-case word)) (sort (lower-case potential-anagram))))
-
 (defn anagram? [word potential-anagram] 
   (and
-    (same-word? word potential-anagram)
-    (same-letters? word potential-anagram)))
+    (not= (lower-case word) (lower-case potential-anagram))
+    (= (sort (lower-case word)) (sort (lower-case potential-anagram)))))
 
 (defn anagrams-for [word, potential-anagrams]
   (filter (partial anagram? word) potential-anagrams))
